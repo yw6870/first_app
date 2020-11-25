889a1e0ceb98f41f268b6e3a4cf46a1d1067deaa
yw6870 <68420090+yw6870@users.noreply.github.com>
2020-11-25 10:38:09 +0900

    postsコントローラー作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,4 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all  # すべてのレコードを@postsに代入
   end
-
-  def new
-  end
-
-  def create
-    Post.create(content: params[:content])
-  end
-end
+ end
